67bf298610bbb2710fc3593cf2a8e55970def8d2
code-with-saad <saadkashif770@gmail.com>
2024-10-11 12:49:18 +0500

    changes

--- app.js
+++ app.js
@@ -32,3 +32,29 @@ function raf(time) {
 }
 
 requestAnimationFrame(raf)
+
+
+
+AOS.init();
+
+
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+
+const navLink = document.querySelectorAll(".nav-item");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
